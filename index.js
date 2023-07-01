3f96fcae3d1750129f27430492ab4040cad7eb69
tkoiko <tynroar@gmail.com>
2023-07-01 17:28:42 +0300

    port fix

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function getItemsList() {
 
 function main() {
   const app = express()
-  const port = 3000
+  const port = process.env.PORT || 3000;
 
   app.use('/' ,express.static(__dirname + '/static'))
 
